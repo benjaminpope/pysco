1fdc40a7b513c61b0d07aeef3281c54db3b4995a
benjaminpope <benjamin.pope@astro.ox.ac.uk>
2015-05-29 00:34:34 +0100

    bug

--- mk_bispec.py
+++ mk_bispec.py
@@ -4,11 +4,12 @@ import whisky as pysco
 
 from time import time
 
-a = pysco.kpi('./geometry/medcross.txt',bsp_mat = 'full',verbose=True)
+a = pysco.kpi('./geometry/medcross.txt', bsp_mat = 'full',verbose=True)
 
 a.save_to_file('./geometry/medcrossmodel_full.pick')
 
-plt.hist(np.log10(a.bsp_s)
+plt.clf()
+plt.hist(np.log10(a.bsp_s))
 plt.xlabel('log10 (singular values)')
 plt.ylabel('N')
 plt.title('SVD Histogram')
