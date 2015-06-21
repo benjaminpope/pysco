86dcf83b7dd7b18a40ddd8f614ba0f3642c76bec
benjaminpope <benjamin.pope@astro.ox.ac.uk>
2015-06-21 20:50:22 +0100

    adding new stuff

---
+++ whisky/frebin.py
@@ -0,0 +1,226 @@
+'''Frebin and pixel rescaling function
+Written by Simon Zieleniewski (frebin copied from IDL)
+
+Last modified 27-09-13
+'''
+
+import numpy as n
+import pyfits as p
+import scipy as s
+
+def rebin(a, new_shape):
+    """
+    Resizes a 2d array by averaging or repeating elements, 
+    new dimensions must be integral factors of original dimensions
+ 
+    Parameters
+    ----------
+    a : array_like
+        Input array.
+    new_shape : tuple of int
+        Shape of the output array (y, x)
+ 
+    Returns
+    -------
+    rebinned_array : ndarray
+        If the new shape is smaller of the input array, the data are averaged, 
+        if the new shape is bigger array elements are repeated
+ 
+    See Also
+    --------
+    resize : Return a new array with the specified shape.
+ 
+    Examples
+    --------
+    >>> a = np.array([[0, 1], [2, 3]])
+    >>> b = rebin(a, (4, 6)) #upsize
+    >>> b
+    array([[0, 0, 0, 1, 1, 1],
+           [0, 0, 0, 1, 1, 1],
+           [2, 2, 2, 3, 3, 3],
+           [2, 2, 2, 3, 3, 3]])
+ 
+    >>> c = rebin(b, (2, 3)) #downsize
+    >>> c
+    array([[ 0. ,  0.5,  1. ],
+           [ 2. ,  2.5,  3. ]])
+ 
+    """
+    M, N = a.shape
+    m, nn = new_shape
+    if m<M:
+        return a.reshape((m,M/m,nn,N/nn)).mean(3).mean(1)
+    else:
+        return n.repeat(n.repeat(a, m/M, axis=0), nn/N, axis=1)
+
+
+    
+
+def frebin(array, shape, total=True):
+    '''Function that performs flux-conservative
+    rebinning of an array.
+
+    Inputs:
+        array: numpy array to be rebinned
+        shape: tuple (x,y) of new array size
+	total: Boolean, when True flux is conserved
+
+    Outputs:
+	new_array: new rebinned array with dimensions: shape
+    '''
+
+    #Determine size of input image
+    y, x = array.shape
+
+    y1 = y-1
+    x1 = x-1
+
+    xbox = x/float(shape[0])
+    ybox = y/float(shape[1])
+
+    #Determine if integral contraction so we can use rebin
+    if (x == int(x)) and (y == int(y)):
+        if (x % shape[0] == 0) and (y % shape[1] == 0):
+            return rebin(array, (shape[1], shape[0]))*xbox*ybox
+
+    #Otherwise if not integral contraction
+    #First bin in y dimension
+    temp = n.zeros((shape[1], x),dtype=float)
+    #Loop on output image lines
+    for i in xrange(0, int(shape[1]), 1):
+        rstart = i*ybox
+        istart = int(rstart)
+        rstop = rstart + ybox
+        istop = int(rstop)
+        if istop > y1:
+            istop = y1
+        frac1 = rstart - istart
+        frac2 = 1.0 - (rstop - istop)
+        
+    #Add pixel values from istart to istop an subtract
+    #fracion pixel from istart to rstart and fraction
+    #fraction pixel from rstop to istop.
+        if istart == istop:
+            temp[i,:] = (1.0 - frac1 - frac2)*array[istart,:]
+        else:
+            temp[i,:] = n.sum(array[istart:istop+1,:], axis=0)\
+                        - frac1*array[istart,:]\
+                        - frac2*array[istop,:]
+            
+    temp = n.transpose(temp)
+
+    #Bin in x dimension
+    result = n.zeros((shape[0], shape[1]), dtype=float)
+    #Loop on output image samples
+    for i in xrange(0, int(shape[0]), 1):
+        rstart = i*xbox
+        istart = int(rstart)
+        rstop = rstart + xbox
+        istop = int(rstop)
+        if istop > x1:
+            istop = x1
+        frac1 = rstart - istart
+        frac2 = 1.0 - (rstop - istop)
+    #Add pixel values from istart to istop an subtract
+    #fracion pixel from istart to rstart and fraction
+    #fraction pixel from rstop to istop.
+        if istart == istop:
+            result[i,:] = (1.-frac1-frac2)*temp[istart,:]
+        else:
+            result[i,:] = n.sum(temp[istart:istop+1,:], axis=0)\
+                          - frac1*temp[istart,:]\
+                          - frac2*temp[istop,:]
+
+    if total:
+        return n.transpose(result)
+    elif not total:
+        return n.transpose(result)/float(xbox*ybox)
+
+
+#Datacube Spaxel rescaling
+def fits_spaxel_scale(fits_image, spaxel, new_file):
+    '''Function that takes image and rebins it to chosen
+    spaxel scale. Reads spaxel scale of input image
+    from header keywords CDELT1/2 - UNITS MUST BE 'MAS'. 
+
+    Inputs:
+        image: science image FITS file
+        spaxel: spaxel scale mas (x, y)
+        new_file: new FITS filename and path
+
+    Outputs:
+        new_cube: datacube rebinned to chosen spaxel scale
+        new_head: updated header file
+    '''
+
+    print 'Spaxel scale'
+
+    image, head = p.getdata(fits_image, 0, header=True)
+    y, x = image.shape
+    cdelt1 = head["CDELT1"]
+    cdelt2 = head["CDELT2"]
+
+    #total field of view in mas
+    x_field = cdelt1*x
+    y_field = cdelt2*y
+
+    x_newsize = x_field/float(spaxel[0])
+    y_newsize = y_field/float(spaxel[1])
+
+    new_im = n.zeros((y_newsize, x_newsize), dtype=float)
+
+    print (x_newsize, y_newsize)
+
+    new_im[:,:] = frebin(image, (x_newsize, y_newsize), total=True)
+
+    head.update('CDELT1', spaxel[0], "mas")
+    head.update('CDELT2', spaxel[1], "mas")
+
+    print 'Spaxel scale - done!'
+
+    p.writeto(str(new_file), new_im, head) 
+    
+ #   return new_im, head
+
+#Datacube Spaxel rescaling
+def spaxel_scale(image, pscale, spaxel):
+    '''Function that takes image and rebins it to chosen
+    spaxel scale. Reads spaxel scale of input image
+    from header keywords CDELT1/2 - UNITS MUST BE 'MAS'. 
+
+    Inputs:
+        image: science image FITS file
+        spaxel: spaxel scale mas (x, y)
+        new_file: new FITS filename and path
+
+    Outputs:
+        new_cube: datacube rebinned to chosen spaxel scale
+        new_head: updated header file
+
+    This is Ben's hack to get it to take ordinary arrays
+    '''
+
+    #print 'Spaxel scale'
+
+    y, x = image.shape
+    cdelt1 = pscale #hacky hack hack
+    cdelt2 = pscale
+
+    #total field of view in mas
+    x_field = cdelt1*x
+    y_field = cdelt2*y
+
+    x_newsize = x_field/float(spaxel)
+    y_newsize = y_field/float(spaxel)
+
+    new_im = n.zeros((y_newsize, x_newsize), dtype=float)
+
+    # print (x_newsize, y_newsize)
+
+    new_im[:,:] = frebin(image, (x_newsize, y_newsize), total=True)
+
+    print 'Spaxel scale - done!'
+
+    # p.writeto(str(new_file), new_im, head) 
+    
+    return new_im
---
+++ whisky/simpupil.py
@@ -0,0 +1,88 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pyfits as pf
+from scipy.interpolate import RectBivariateSpline as interp 
+import Image
+import ImageDraw
+import pickle
+
+def palomarpupil(sz=1024,spiders=True):
+	'''------------------------------------------------
+	simpupil.py 
+
+	generates a Palomar pupil
+
+	Owes a lot to Anthony Cheetham's IDL script
+	------------------------------------------------'''
+
+	#things to edit
+	rprim = 5.093/2.  * 15.4/16.88 
+	rsec = 1.829/2. * 7.6/6.08 
+	spiderthick = 0.83 *15.4/16.88/4.
+
+	'''-------------------------------------------
+	Initialise your arrays
+	-------------------------------------------'''
+
+	mask = np.ones((sz,sz))
+
+	# get coordinates for the full un-cropped image
+	xs, ys = np.linspace(-sz/2,sz/2,sz), np.linspace(-sz/2,sz/2,sz)
+	xs *= 9*rprim/xs.max()
+	ys *= 9*rprim/ys.max()
+
+	xx,yy = np.meshgrid(xs,ys)
+	rr = np.sqrt(xx**2 + yy**2)
+
+	m2pix = sz/(xx.max()-xx.min())
+	pix2m = 1./m2pix
+
+	'''-------------------------------------------
+	Put in the primary and secondary sizes
+	-------------------------------------------'''
+
+	mask /= mask.max()
+	mask[rr>rprim] = 0
+	mask[rr<rsec] = 0
+
+	'''-------------------------------------------
+	Now do the spiders
+	-------------------------------------------'''
+	if spiders:
+		angles = [0., 90., 180., 270.]
+
+		img = Image.fromarray(mask)
+		draw = ImageDraw.Draw(img)
+
+		for j, angle in enumerate(angles):
+			
+			angle *= np.pi/180.
+
+			start = 1.1*rprim*np.array([np.cos(angle),np.sin(angle)])-xx.min()
+			stop = np.array([0,0])-xx.min()
+
+			normvec = np.array([start[1]-stop[1],stop[0]-start[0]])*spiderthick
+			corners = np.round(m2pix*np.array([start+normvec,start-normvec,stop-normvec,stop+normvec]))
+
+			#convert to pixels
+
+			draw.polygon([tuple(p) for p in corners], fill=0)
+
+		newmask = np.copy(np.asarray(img))
+	else:
+		newmask = np.copy(mask)
+
+
+	'''-------------------------------------------
+	Display
+	-------------------------------------------'''
+
+	# plt.figure(0)
+	# plt.imshow(newmask,cmap=plt.cm.gray,extent=[xx.min(),xx.max(),xx.min(),xx.max()])
+	# plt.xlabel('x (m)')
+	# plt.ylabel('y (m)')
+	# plt.title('WFIRST Pupil')
+	# plt.draw()
+	# plt.show()
+
+	return newmask,xs,m2pix
