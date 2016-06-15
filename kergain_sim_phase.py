1ab4f69a8e3e048833e5939dcab43aa1db20d68d
benjaminpope <benjamin.pope@astro.ox.ac.uk>
2016-06-15 18:20:48 +0100

    do more

--- kergain_sim_phase.py
+++ kergain_sim_phase.py
@@ -112,7 +112,7 @@ Loop over a range of contrasts
 ----------------------------------------'''
 
 contrast_list =  [10,50,100,150,200,250,300,350,400,450,500]
-contrast_list =  [10,50,100,200,300,400,500]
+# contrast_list =  [10,50,100,200,300,400,500]
 ncalcs = len(contrast_list)# * nframes
 
 kseps, kthetas, kcons = np.zeros(ncalcs), np.zeros(ncalcs), np.zeros(ncalcs)
