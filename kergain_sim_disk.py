2258284199e6bec3c6883ab97e2bfb10b520d735
benjaminpope <benjamin.pope@astro.ox.ac.uk>
2016-07-21 19:58:58 +1000

    even further

--- kergain_sim_disk.py
+++ kergain_sim_disk.py
@@ -197,7 +197,7 @@ psfs = np.zeros((nimages,imsz,imsz))
 Loop over a range of contrasts
 ----------------------------------------'''
 
-contrast_list = np.linspace(200,1000.,5)
+contrast_list = np.linspace(1000,3000.,10)
 
 ncalcs = len(contrast_list)
 
